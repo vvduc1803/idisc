ac5a33ba9cd71d0b043388dc731ec67d5e4f51ac
van_duc <vvduc03@gmail.com>
2023-08-07 08:46:56 +0700

    update

---
+++ idisc/dataloders/__init__.py
@@ -0,0 +1,21 @@
+from .argoverse import ArgoverseDataset
+from .dataset import BaseDataset
+from .ddad import DDADDataset
+from .diode import DiodeDataset
+from .kitti import KITTIDataset
+from .nyu import NYUDataset
+from .nyu_normals import NYUNormalsDataset
+from .sunrgbd import SUNRGBDDataset
+from .ycbv import YCBVDataset
+
+__all__ = [
+    "BaseDataset",
+    "NYUDataset",
+    "NYUNormalsDataset",
+    "KITTIDataset",
+    "ArgoverseDataset",
+    "DDADDataset",
+    "DiodeDataset",
+    "SUNRGBDDataset",
+    "YCBVDataset",
+]
